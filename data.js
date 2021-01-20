fcd4c5b1735db7f251a6ecaf699ec31f4c7684d7
dingeHCHS <dalton.inge@henderson.kyschools.us>
2021-01-20 11:55:46 -0600

    Culinary

---
+++ data.js
@@ -0,0 +1,73 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-culinary",
+      "name": "Culinary",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.1402539253422503,
+        "pitch": -0.0077265453653048155,
+        "fov": 1.3721846102296735
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.9700130566953042,
+          "pitch": 0.03144444849631789,
+          "title": "CulinaryIntro",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.4107314412268765,
+          "pitch": 0.12299739947586374,
+          "title": "Culinary1",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.901808864472132,
+          "pitch": 0.12442582024436177,
+          "title": "Culinary2",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.1199722995206827,
+          "pitch": 0.18932177253843463,
+          "title": "Culinary3",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.0263543978698113,
+          "pitch": -0.10429478635644429,
+          "title": "Culinary4",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
